769807dcd591a6d3498c2e5892f8a4e62f62b0a4
poiuytrewq0987654321 <bundushathurh@gmail.com>
2019-05-14 23:41:06 +0200

    update

--- repo.sh
+++ repo.sh
@@ -2,11 +2,11 @@
 # dodanie repo do pliku sources.list
 
 echo 'dodawanie wpisow repo do sources.list'
-cd /etc/apt/sources.list
-sed -r sources.list
-'deb http://deb.debian.org/debian/ stretch main contrib' >> sources.list
-'deb http://security.debian.org/ stretch/updates contrib main' >> sources.list
-'deb http://deb.debian.org/debian/ stretch-updates contrib main' >> sources.list
+cd /etc/apt
+sed '1,50d' sources.list
+echo 'deb http://deb.debian.org/debian/ stretch main contrib' >> sources.list
+echo 'deb http://security.debian.org/ stretch/updates contrib main' >> sources.list
+echo 'deb http://deb.debian.org/debian/ stretch-updates contrib main' >> sources.list
 
 echo 'updateowanie repo'
 apt-get update
@@ -19,4 +19,4 @@ apt-get install sudo
 
 echo 'nadawanie uprawnien sudo uzytkownikom'
 cd /etc
-'student ALL=(ALL:ALL) ALL' >> sudoers
+echo 'student ALL=(ALL:ALL) ALL' >> sudoers
