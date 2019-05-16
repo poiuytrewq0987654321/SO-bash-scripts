fc874003609dbd4457b332b7bbafbb5506257672
Horrid Wilting <ba@horridwilting.pl>
2019-05-16 20:40:51 +0200

    add apache.sh (alpha version)

---
+++ apache.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo 'instalacja apache2'
+apt-get -y install apache2
+
+echo 'uruchomienie apache2'
+systemctl start apache2
+
+znajdz=$(ifconfig | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+echo 'aby sprawdzic czy apache2 dziala poprawnie, wejdz na innej maszynie wirtualnej na adres:'
+echo $znajdz
