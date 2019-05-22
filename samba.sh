70edef192f12a5b4daaa33c948dc7a1c376ff83d
Horrid Wilting <ba@horridwilting.pl>
2019-05-22 20:04:56 +0200

    update samba.sh

--- samba.sh
+++ samba.sh
@@ -23,12 +23,11 @@ if [[ $znajdz = 1 ]]; then
     chmod 777 /media/storage
 fi
 
-echo 'restart samba service'
-systemctl restart smbd.service
-
 echo 'konfiguracja samby pod uwierzytelnienie'
 echo 'dla usera: smbuser i hasle: smbuser123'
-useradd -p $(openssl passwd -1 $smbuser123) $smbuser
+useradd -p smbuser123 smbuser
+
+awk -F: '{ print $1}' /etc/passwd
 
 echo 'konfiguracja pliku smb.conf dla udzialu restricted (logowanie poprzez usera)'
 echo '[restricted]' >> /etc/samba/smb.conf
@@ -42,13 +41,12 @@ echo 'guest ok = no' >> /etc/samba/smb.conf
 echo 'create mask = 0600' >> /etc/samba/smb.conf
 echo 'directory mask = 0700' >> /etc/samba/smb.conf
 
+echo 'restart samba service'
+systemctl restart smbd.service
+
 cd /media
 znajdz2=$(find /media/ -name 'storage2' | wc -l)
 
-if [[ $znajdz2 = 1 ]]; then
-    chmod 777 /media/storage2
-    else
-    mkdir /media/storage2
-    chown -R smbuser:smbuser /media/storage2
-    chmod 700 /media/storage2
-fi
+mkdir /media/storage2
+chown -R smbuser:smbuser /media/storage2
+chmod 700 /media/storage2
