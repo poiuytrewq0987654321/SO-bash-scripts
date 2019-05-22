e566df8f83a5d1de1008c2f89a4aa7691bf83b6f
Horrid Wilting <ba@horridwilting.pl>
2019-05-22 20:43:02 +0200

    update samba.sh

--- samba.sh
+++ samba.sh
@@ -3,7 +3,21 @@
 echo 'instalacja samby'
 apt-get -y install samba
 
-echo 'konfiguracja pliku smb.conf dla udzialu public'
+rm /etc/samba/smb.conf
+touch /etc/samba/smb.conf
+
+echo 'dodawanie konfiguracji do pliku smb.conf'
+echo '[global]' >> /etc/samba/smb.conf
+echo 'workgroup = smb' >> /etc/samba/smb.conf
+echo 'security = user' >> /etc/samba/smb.conf
+echo 'map to guest = never' >> /etc/samba/smb.conf
+
+echo '[homes]' >> /etc/samba/smb.conf
+echo 'comment = Home Directories' >> /etc/samba/smb.conf
+echo 'browsable = no' >> /etc/samba/smb.conf
+echo 'read only = no' >> /etc/samba/smb.conf
+echo 'create mode = 0750' >> /etc/samba/smb.conf
+
 echo '[public]' >> /etc/samba/smb.conf
 echo 'path = /media/storage/' >> /etc/samba/smb.conf
 echo 'public = yes' >> /etc/samba/smb.conf
@@ -12,24 +26,6 @@ echo 'comment = smb share' >> /etc/samba/smb.conf
 echo 'printable = no' >> /etc/samba/smb.conf
 echo 'guest ok = yes' >> /etc/samba/smb.conf
 
-echo 'tworzenie (jesli go nie ma) folderu storage i nadawanie mu uprawnien'
-cd /media
-znajdz=$(find /media/ -name 'storage' | wc -l)
-
-if [[ $znajdz = 1 ]]; then
-    chmod 777 /media/storage
-    else
-    mkdir /media/storage
-    chmod 777 /media/storage
-fi
-
-echo 'konfiguracja samby pod uwierzytelnienie'
-echo 'dla usera: smbuser i hasle: smbuser123'
-useradd -p smbuser123 -s /bin/false smbuser
-
-awk -F: '{ print $1}' /etc/passwd
-
-echo 'konfiguracja pliku smb.conf dla udzialu restricted (logowanie poprzez usera)'
 echo '[restricted]' >> /etc/samba/smb.conf
 echo 'valid users = smbuser' >> /etc/samba/smb.conf
 echo 'path = /media/storage2/' >> /etc/samba/smb.conf
@@ -40,6 +36,15 @@ echo 'printable = no' >> /etc/samba/smb.conf
 echo 'guest ok = no' >> /etc/samba/smb.conf
 echo 'create mask = 0600' >> /etc/samba/smb.conf
 echo 'directory mask = 0700' >> /etc/samba/smb.conf
+
+mkdir /media/storage
+chmod 777 /media/storage
+
+echo 'konfiguracja samby pod uwierzytelnienie'
+echo 'dla usera: smbuser i hasle: smbuser123'
+useradd -p smbuser123 -s /bin/false smbuser
+
+awk -F: '{ print $1}' /etc/passwd
 
 echo 'restart samba service'
 systemctl restart smbd.service
