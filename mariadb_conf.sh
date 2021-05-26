f40c8a1cd71103d78f7c33ae88d2b7a0081434a7
Horrid Wilting <contact@horridwilting.pl>
2021-05-26 20:08:04 +0200

    update

--- mariadb_conf.sh
+++ mariadb_conf.sh
@@ -28,9 +28,6 @@ expect eof
 
 echo "$SECURE_MYSQL"
 
-#enter, n enter, y enter, y enter, y enter, y enter,
-#printf '\nn\ny\ny\ny\ny\n'
-
 printf "sudo mysql -u root -p\n\n"
 printf "CREATE DATABASE wpdb;\n"
 printf "GRANT ALL ON wpdb.* TO 'wpuser' IDENTIFIED BY 'wppasword';\n"
