4daada7684059b224064f8f2064c70cd8dc7f3ed
Horrid Wilting <contact@horridwilting.pl>
2021-05-26 15:50:20 +0200

    update mariadb-conf

--- mariadb_conf.sh
+++ mariadb_conf.sh
@@ -8,7 +8,7 @@ sudo mysql_secure_installation
 #enter, n enter, y enter, y enter, y enter, y enter, 
 #printf '\nn\ny\ny\ny\ny\n'
 
-printf "sudo mysql -u root -p\n"
+printf "sudo mysql -u root -p\n\n"
 printf "CREATE DATABASE wpdb;\n"
 printf "GRANT ALL ON wpdb.* TO 'wpuser' IDENTIFIED BY 'wppasword';\n"
 printf "FLUSH PRIVILEGES;\n"
